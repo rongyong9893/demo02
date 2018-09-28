5d5beedef343ee7676c043c55720f7ed4cf26cbc
rong <rongyong9893@163.com>
2018-09-29 00:40:43 +0800

    登陆界面国际化

---
+++ src/main/java/com/example/demo/component/MyLocaleResolver.java
@@ -0,0 +1,34 @@
+package com.example.demo.component;
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ *
+ *
+*/
+public class MyLocaleResolver implements LocaleResolver
+{
+    @Override
+    public Locale resolveLocale(HttpServletRequest request)
+    {
+        Locale locale = Locale.getDefault();
+        String l = request.getParameter("l");
+
+        if (!StringUtils.isEmpty(l))
+        {
+            String[] split = l.split("_");
+            locale = new Locale(split[0],split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
--- src/main/java/com/example/demo/config/MyMvConfig.java
+++ src/main/java/com/example/demo/config/MyMvConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.config;
 
+import com.example.demo.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -21,6 +23,12 @@ public class MyMvConfig extends WebMvcConfigurerAdapter {
        };
 
        return adapter;
+   }
+
+   //国际化
+   @Bean
+    public LocaleResolver localeResolver(){
+       return new MyLocaleResolver();
    }
 
 }
